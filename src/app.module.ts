6735cd963efcb8839c8081653672a1ce70b9a4cc
Ahmed Ayman <ahmed1997164@gmail.com>
2024-04-15 15:35:33 +0200

    remove console.log
    Took 31 minutes

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,21 +12,10 @@ import { SeedModule } from './seed/seed.module';
 import configuration from './config/configuration';
 import { validate } from '../env.validation';
 
-console.log('process.env', process.env);
-console.log('process.env.APP_HOST', process.env.APP_HOST);
-console.log('process.env.APP_PORT', process.env.APP_PORT);
-console.log('process.env.JWT_SECRET', process.env.JWT_SECRET);
-console.log('process.env.NODE_ENV', process.env.NODE_ENV);
-console.log('process.env.DATABASE_HOST', process.env.DATABASE_HOST);
-console.log('process.env.DATABASE_PORT', process.env.DATABASE_PORT);
-console.log('process.env.DATABASE_USERNAME', process.env.DATABASE_USERNAME);
-console.log('process.env.DATABASE_PASSWORD', process.env.DATABASE_PASSWORD);
-console.log('process.env.DATABASE_NAME', process.env.DATABASE_NAME);
-
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: [`${process.cwd()}/.env.${process.env.NODE_ENV}.local`, `${process.cwd()}/.env.${process.env.NODE_ENV}`, '.env'],
+      envFilePath: [`${process.cwd()}/.env.${process.env.NODE_ENV}.local`, `${process.cwd()}/.env.${process.env.NODE_ENV}`],
       ignoreEnvFile: process.env.NODE_ENV === 'production' || process.env.RAILWAY_ENVIRONMENT_NAME === 'production',
       isGlobal: true,
       load: [configuration],
